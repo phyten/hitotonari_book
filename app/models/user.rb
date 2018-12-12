c1c76c2414516f0296303db3ba4e9412fddcad19
c.o <carina729.ai@ezweb.ne.jp>
2018-12-12 04:52:33 +0000

    User model

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+  #パスワードセキュリティ
+  has_secure_password
+  
+  #バリデーション
+  validates :name, presence: true, length: { maximum:50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+                    
+  #Modelの関係づけ
+  has_many :books
+  
+  #textコピペ
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  #フォロー機能（textコピペ）
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+  
+  #def friends_booksをあとで記入
+  
+end
